68fd54bdf86219900cddaf676e6b228749c50c34
DaveatCor <condaveat@gmail.com>
2018-11-08 09:59:11 +0700

    Done

--- POS/Login/Form1.Designer.cs
+++ POS/Login/Form1.Designer.cs
@@ -31,8 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.passWord = new System.Windows.Forms.TextBox();
+            this.UserName = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -60,8 +60,8 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.passWord);
+            this.panel1.Controls.Add(this.UserName);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
@@ -71,20 +71,20 @@
             this.panel1.Size = new System.Drawing.Size(334, 249);
             this.panel1.TabIndex = 2;
             // 
-            // textBox2
+            // passWord
             // 
-            this.textBox2.Location = new System.Drawing.Point(62, 160);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(233, 20);
-            this.textBox2.TabIndex = 5;
+            this.passWord.Location = new System.Drawing.Point(62, 160);
+            this.passWord.Name = "passWord";
+            this.passWord.PasswordChar = '*';
+            this.passWord.Size = new System.Drawing.Size(233, 20);
+            this.passWord.TabIndex = 5;
             // 
-            // textBox1
+            // UserName
             // 
-            this.textBox1.Location = new System.Drawing.Point(62, 73);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(233, 20);
-            this.textBox1.TabIndex = 4;
+            this.UserName.Location = new System.Drawing.Point(62, 73);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(233, 20);
+            this.UserName.TabIndex = 4;
             // 
             // button2
             // 
@@ -104,6 +104,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -137,8 +138,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox passWord;
+        private System.Windows.Forms.TextBox UserName;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
